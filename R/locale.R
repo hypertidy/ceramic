d6638cb78df88abc6c82dd43de4aaec910757e8c
Michael Sumner <mdsumner@gmail.com>
2018-11-12 11:15:44 +0000

    locations

--- R/locale.R
+++ R/locale.R
@@ -7,10 +7,7 @@ fast_merge <- function(x) {
   raster::setValues(raster::brick(out, out, out), vals[order(cells), ])
 }
 
-cc_location <- function(loc = NULL, buffer = 5000,
-                        type = "mapbox.outdoors", ..., debug = FALSE) {
-  get_loc(loc = loc, buffer = buffer, type = type, ..., debug = debug)
-}
+
 #' Miscellaneous places
 #'
 #' Visit some nice locales with web tiles.
@@ -18,22 +15,28 @@ cc_location <- function(loc = NULL, buffer = 5000,
 #' * `cc_macquarie` Macquarie Island
 #'
 #' @param buffer with in metres to extend around the location
-#' @param ...
+#' @param ... dots, ignored currently
 #' @param debug optionally print out files that will be used
 #'
 #' @return RasterBrick, with RGB
 #' @export
-#' @name ceramic_locale
+#' @name cc_location
 #' @examples
 #' im <- cc_macquarie()
 #' library(raster)
 #' plotRGB(im)
+cc_location <- function(loc = NULL, buffer = 5000,
+                        type = "mapbox.outdoors", ..., debug = FALSE) {
+  get_loc(loc = loc, buffer = buffer, type = type, ..., debug = debug)
+}
+#' @name cc_location
+#' @export
 cc_macquarie <- function(loc = c(158.93835,-54.49871), buffer = 5000,
                          type = "mapbox.outdoors", ..., debug = FALSE) {
  cc_location(loc, buffer, type = type, ..., debug = debug)
 }
 
-#' @name ceramic_locale
+#' @name cc_location
 #' @export
 cc_davis <- function(loc = c(77 + 58/60 + 3/3600,
                               -(68 + 34/60 + 36/3600)),
@@ -42,7 +45,7 @@ cc_davis <- function(loc = c(77 + 58/60 + 3/3600,
   cc_location(loc, buffer, type = type, ..., debug = debug)
 
 }
-#' @name ceramic_locale
+#' @name cc_location
 #' @export
 cc_mawson <- function(loc = c(62 + 52/60 + 27/3600,
                                   -(67 + 36/60 + 12/3600)), buffer = 5000, type = "mapbox.outdoors", ..., debug = FALSE) {
@@ -52,7 +55,7 @@ cc_mawson <- function(loc = c(62 + 52/60 + 27/3600,
 
 }
 
-#' @name ceramic_locale
+#' @name cc_location
 #' @export
 cc_casey <- function(  loc = cbind(110 + 31/60 + 36/3600,
                                     -(66 + 16/60 + 57/3600)), buffer = 5000, type = "mapbox.outdoors", ..., debug = FALSE) {
@@ -61,7 +64,7 @@ cc_casey <- function(  loc = cbind(110 + 31/60 + 36/3600,
   cc_location(loc, buffer, type = type, ..., debug = debug)
 
 }
-#' @name ceramic_locale
+#' @name cc_location
 #' @export
 cc_heard <- function(loc = c(73 + 30/60 + 30/3600,
                                  -(53 + 0 + 0/3600)), buffer = 5000, type = "mapbox.outdoors",..., debug = FALSE) {
@@ -70,7 +73,7 @@ cc_heard <- function(loc = c(73 + 30/60 + 30/3600,
   cc_location(loc, buffer, type = type, ..., debug = debug)
 
 }
-#' @name ceramic_locale
+#' @name cc_location
 #' @export
 cc_kingston <- function(loc = c(-147.70837,
                                     -42.98682), buffer = 5000, type = "mapbox.outdoors", ..., debug = FALSE) {
