cfd91d0dbfc02371947cf57f8ca34494fdc79ec2
Michael Sumner <mdsumner@gmail.com>
2019-03-24 23:44:17 +0000

    app

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,6 +3,7 @@ library(ceramic)
 ## default public key  https://account.mapbox.com/access-tokens/
 if (identical(Sys.getenv("APPVEYOR"), "True")) {
   Sys.setenv(MAPBOX_API_KEY="pk.eyJ1IjoibWRzdW1uZXIiLCJhIjoiY2lleHM3dDk5MDBzbHM4bTM1bjkyY2kwayJ9.IrUihishnOa8w0JFXhXqZA")
+  print(get_api_key())
 }
   test_check("ceramic")
 
