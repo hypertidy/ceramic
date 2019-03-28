51da6176137c208b4d5b977719e6186ad50a95b8
Michael Sumner <mdsumner@gmail.com>
2019-03-29 07:13:08 +1100

    print debuggin

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,10 +1,6 @@
 library(testthat)
 library(ceramic)
-##   https://account.mapbox.com/access-tokens/
-if (identical(Sys.getenv("APPVEYOR"), "True")) {
-  #Sys.setenv(MAPBOX_API_KEY="pk.eyJ1IjoibWRzdW1uZXIiLCJhIjoiY2p0bDI1aGY1MTRiNDQ0bWR2djh4dzgxOSJ9.zPM71aZwRWHc9U5kvDQDIA")
-  print(ceramic:::get_api_key())
-}
+
 test_check("ceramic")
 
 # if (identical(Sys.getenv("TRAVIS"), "true")) {
--- tests/testthat/test-cache.R
+++ tests/testthat/test-cache.R
@@ -1,10 +1,23 @@
 context("test-cache")
 
-test_that("caching is sensible", {
-  x <- cc_location(cbind(0, 0), debug = TRUE)
+##   https://account.mapbox.com/access-tokens/
+if (identical(Sys.getenv("APPVEYOR"), "True")) {
+  #Sys.setenv(MAPBOX_API_KEY="pk.eyJ1IjoibWRzdW1uZXIiLCJhIjoiY2p0bDI1aGY1MTRiNDQ0bWR2djh4dzgxOSJ9.zPM71aZwRWHc9U5kvDQDIA")
+  print("attempt print key on APPVEYOR")
+  print(ceramic:::get_api_key())
+}
+if (identical(Sys.getenv("TRAVIS"), "true")) {
+  #Sys.setenv(MAPBOX_API_KEY="pk.eyJ1IjoibWRzdW1uZXIiLCJhIjoiY2p0bDI1aGY1MTRiNDQ0bWR2djh4dzgxOSJ9.zPM71aZwRWHc9U5kvDQDIA")
+  print("attempt print key on TRAVIS")
+  print(ceramic:::get_api_key())
+}
 
+
+test_that("caching is sensible", {
+  ## this hits zoom 13 and should give 16 tiles
+  x <- cc_location(cbind(0, 0), debug = TRUE)
   expect_s3_class(f <- ceramic_tiles(zoom = 13), "tbl_df")
-  expect_true(nrow(f) > 40)
+  expect_true(nrow(f) > 15)
 
   ## turn off temporarily
   # ## clobber the cache
