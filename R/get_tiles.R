089f1f2df69f51af214c22aa6c81762cd64e4a5c
Michael Sumner <mdsumner@gmail.com>
2020-06-07 18:05:54 +1000

    cleanup

--- R/get_tiles.R
+++ R/get_tiles.R
@@ -89,7 +89,7 @@ get_tiles <- function(x, buffer, type = "mapbox.satellite", crop_to_buffer = TRU
     query_string <- mk_query_string_custom(baseurl = base_url)
   }
 
-
+print(query_string)
   files <- unlist(down_loader(tile_grid, query_string, debug = debug, verbose = verbose))
   bad <- file.info(files)$size < 35
 
