7542bd93150fa71bb006d06e24c294d30245183a
Michael Sumner <mdsumner@gmail.com>
2023-04-21 20:04:06 +1000

    run on gh?

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,7 +3,7 @@ library(ceramic)
 
 ##   https://account.mapbox.com/access-tokens/
 ceramic_key <- "pk.eyJ1IjoibWRzdW1uZXIiLCJhIjoiY2p0bDI1aGY1MTRiNDQ0bWR2djh4dzgxOSJ9.zPM71aZwRWHc9U5kvDQDIA"
-if (identical(Sys.getenv("GITHUB"), "true")) {
+if (isTRUE(as.logical(Sys.getenv("CI")))) {
   Sys.setenv(MAPBOX_API_KEY=ceramic_key)
 }
 
