395a9ff4856dc8b8e24c45da5e548afe32cbb4c0
Michael Sumner <mdsumner@gmail.com>
2023-04-21 21:07:55 +1000

    grr

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,12 +1,20 @@
+# This file is part of the standard setup for testthat.
+# It is recommended that you do not modify it.
+#
+# Where should you do additional test configuration?
+# Learn more about the roles of various files in:
+# * https://r-pkgs.org/tests.html
+# * https://testthat.r-lib.org/reference/test_package.html#special-files
+
 library(testthat)
 library(ceramic)
 
 test <- get_api_key()
 
 if (is.null(test) || !nzchar(test)) {
-  .use_public_key()
+  ceramic:::.use_public_key()
+  get_api_key()
 }
 
-test_check("ceramic")
 
-## ceramic::clear_ceramic_cache(clobber = TRUE)
+test_check("ceramic")
